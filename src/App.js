d30eba074a32fe21a3e14523acdefa590f90759b
motaz <motaz.dn.ali@gmail.com>
2022-02-11 20:21:36 +0300

    Handling the Http Errors

--- src/App.js
+++ src/App.js
@@ -6,22 +6,46 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
     const fetchMoviesHandler = async () => {
       setIsLoading(true);
-     const response = await fetch('https://swapi.dev/api/films/')
-     const data = await response.json()
-        
-        const transFormedMovie = data.results.map(movieData =>{
-          return{
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
-        })
-        setMovies(transFormedMovie);
-        setIsLoading(false)
+      setError(null);
+
+      try{
+        const response = await fetch('https://swapi.dev/api/films/')
+        if(!response.ok){
+          throw new Error('Something went wrong here')
+      }
+        const data = await response.json()
+           
+       
+           const transFormedMovie = data.results.map(movieData =>{
+             return{
+               id: movieData.episode_id,
+               title: movieData.title,
+               openingText: movieData.opening_crawl,
+               releaseDate: movieData.release_date
+             }
+           })
+           setMovies(transFormedMovie);
+      } catch(error){
+        setError(error.message)
       };
+      setIsLoading(false)
+
+ 
+      };
+
+      let content = <p>Press the button above to see the movies</p>
+      if(movies.length > 0){
+        content = <MoviesList movies={movies}/>;
+      }
+      if(error){
+        content = <p>{error}</p>;
+      }
+      if(isLoading){
+        content = <p>Loading...</p>;
+      }
 
   return (
     <React.Fragment>
@@ -29,9 +53,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length !== 0 && <MoviesList movies={movies}/>}
-        {!isLoading && movies.length === 0 && <p>Press the button above to see the movies</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
